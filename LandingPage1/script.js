e490e6c6c518b03062e923bcdd4b4e28cf0d16a8
Elaedus-Zak <zakaria777adlouni@gmail.com>
2026-01-17 13:30:09 +0100

    Delete LandingPage1 directory

--- LandingPage1/script.js
+++
@@ -1,7 +0,0 @@
-const menu =document.getElementById("sidemenu");
-const btn1 = document.getElementById("morebutton");
-const btn2 = document.getElementById("closebtn");
-const blurW= document.getElementById("blurer");
-
-btn1.addEventListener("click",function(){menu.classList.add("open");menu.classList.remove("close");blurW.classList.toggle("show");});
-btn2.addEventListener("click",function(){menu.classList.remove("open");menu.classList.add("close");blurW.classList.toggle("show");});
