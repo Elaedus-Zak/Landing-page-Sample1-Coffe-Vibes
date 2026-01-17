ec50498da25d671db2ef46bff63ee85ba7bfa71f
Elaedus-Zak <zakaria777adlouni@gmail.com>
2026-01-17 16:53:05 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ const blurW= document.getElementById("blurer");
 
 btn1.addEventListener("click",function(){menu.classList.add("open");menu.classList.remove("close");blurW.classList.toggle("show");});
 btn2.addEventListener("click",function(){menu.classList.remove("open");menu.classList.add("close");blurW.classList.toggle("show");});
+blurW.addEventListener("click",function(){menu.classList.remove("open");menu.classList.add("close");blurW.classList.toggle("show");});
+document.addEventListener("keydown",function(event){
+    if (event.key ==='Escape'){
+        menu.classList.remove("open");menu.classList.add("close");blurW.classList.remove("show");}
+    }
+);
